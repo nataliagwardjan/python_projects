6150936abaa876bdd3adf21a591b6fff24a48ca5
nataliagwardjan <natalia.gwardjan@gmail.com>
2023-05-30 08:54:36 +0200

    learning django - add some styles to our website

--- django-kurs/movie/settings.py
+++ django-kurs/movie/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies_web'
+    'movies_web',
+    'bootstrapform'
 ]
 
 MIDDLEWARE = [
